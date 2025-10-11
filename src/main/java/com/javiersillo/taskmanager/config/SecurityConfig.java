447dc0c14fbdc79db50c1220e907802d56e9d689
Javier Candia <javi-candia@hotmail.com>
2025-10-11 00:56:32 -0600

    security config updated

--- src/main/java/com/javiersillo/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/javiersillo/taskmanager/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,14 +15,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() // Permite H2 Console
-                        .anyRequest().permitAll() // Permite TODO lo demás (por ahora)
+                        .anyRequest().permitAll() // Deja todo público mientras aprendes Docker/AWS
                 )
-                .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF (común para APIs REST)
-                // .headers(headers -> headers.frameOptions().disable()); // <-- ¡Esta línea está DEPRECATED!
-                .headers(headers -> headers // <-- ¡Nueva forma de configurar los headers!
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // <-- Así se deshabilita frameOptions ahora
-                );
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
